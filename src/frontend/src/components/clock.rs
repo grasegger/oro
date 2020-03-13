d9df43bb8bc9bd54bc641779857b9180a9701ac2
Karl Grasegger <karl.grasegger@gebruederheitz.de>
2020-03-13 19:09:10 +0100

    Moved components to own folder.

---
+++ src/frontend/src/components/clock.rs
@@ -0,0 +1,37 @@
+use yew::{html, Callback, Component, ComponentLink, Html, ShouldRender};
+use web_sys::console;
+
+pub struct Model {
+    link: ComponentLink<Self>,
+}
+
+pub enum Msg {
+    Click,
+}
+
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Model { link }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click => {
+				console::log_1(&"clock".into())
+			}
+        }
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::Click)>{ "Clock" }</button>
+            </div>
+        }
+    }
+}
--- src/frontend/src/lib.rs
+++ src/frontend/src/lib.rs
@@ -7,48 +7,12 @@ extern crate web_sys;
 extern crate yew; 
 
 use wasm_bindgen::prelude::*;
-use yew::{html, Callback, Component, ComponentLink, Html, ShouldRender};
 
-use web_sys::console;
+mod components;
 
 // Called by our JS entry point
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
-    yew::start_app::<Model>();
+    yew::start_app::<components::clock::Model>();
     Ok(())
-}
-
-pub struct Model {
-    link: ComponentLink<Self>,
-}
-
-pub enum Msg {
-    Click,
-}
-
-
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Model { link }
-    }
-
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::Click => {
-				console::log_1(&"clock".into())
-			}
-        }
-        true
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            <div>
-                <button onclick=self.link.callback(|_| Msg::Click)>{ "Clock" }</button>
-            </div>
-        }
-    }
 }

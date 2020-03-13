65ad21cc4d42c8f2fc4e53e370084ce7e66482cd
Karl Grasegger <karl.grasegger@gebruederheitz.de>
2020-03-13 21:00:12 +0100

    Basic form in place

---
+++ src/frontend/src/components/index.rs
@@ -0,0 +1,52 @@
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+use web_sys::console;
+
+pub struct Index {
+    link: ComponentLink<Self>,
+}
+
+pub enum Msg {
+    Click,
+}
+
+
+impl Component for Index {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Index { link }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::Click => {
+				console::log_1(&"clock".into())
+			}
+        }
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div class="nes-container with-title">
+				<h3 class="title">{"Quest 1: Tell Me About Yourself"}</h3>
+				<form>
+					<div class="nes-field">
+						<label for="instance">{"Mite Instance"}</label>
+						<input class="nes-input" name="instance"/>
+					</div>
+
+					<div class="nes-field">
+						<label for="apikey">{"Mite API Key"}</label>
+						<input class="nes-input" type="password" name="apikey"/>
+					</div>
+					
+					<div class="nes-field">
+						<input type="submit" class="nes-btn is-primary" value={"Complete Quest"} onclick=self.link.callback(|_| Msg::Click)/>
+					</div>
+				</form>
+            </div>
+        }
+    }
+}

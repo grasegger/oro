e6998a9166f7ac6fa8ff0503b73db2abb3de1bdf
Karl Grasegger <karl.grasegger@gebruederheitz.de>
2020-03-14 10:02:57 +0100

    Preventing navigation on submit button now.
    Fixes #2

--- src/frontend/src/components/index.rs
+++ src/frontend/src/components/index.rs
@@ -1,4 +1,4 @@
-use yew::{html, Component, ComponentLink, Html, ShouldRender};
+use yew::{html, Component, ComponentLink, Html, ShouldRender, MouseEvent};
 use web_sys::console;
 
 pub struct Index {
@@ -6,7 +6,7 @@ pub struct Index {
 }
 
 pub enum Msg {
-    Click,
+    Click(MouseEvent),
 }
 
 
@@ -18,34 +18,35 @@ impl Component for Index {
         Index { link }
     }
 
-    fn update(&mut self, msg: Self::Message) -> ShouldRender {
-        match msg {
-            Msg::Click => {
-				console::log_1(&"clock".into())
-			}
-        }
-        true
+    fn update(&mut self, msg: Self::Message,) -> ShouldRender {
+	    match msg {
+		    Msg::Click (MouseEvent) => {
+			    MouseEvent.prevent_default();
+			    console::log_1(&"clock".into());
+		    }
+	    }
+	    true
     }
 
     fn view(&self) -> Html {
         html! {
             <div class="nes-container with-title">
-				<h3 class="title">{"Quest 1: Tell Me About Yourself"}</h3>
-				<form>
-					<div class="nes-field">
-						<label for="instance">{"Mite Instance"}</label>
-						<input class="nes-input" name="instance"/>
-					</div>
-
-					<div class="nes-field">
-						<label for="apikey">{"Mite API Key"}</label>
-						<input class="nes-input" type="password" name="apikey"/>
-					</div>
-					
-					<div class="nes-field">
-						<input type="submit" class="nes-btn is-primary" value={"Complete Quest"} onclick=self.link.callback(|_| Msg::Click)/>
-					</div>
-				</form>
+		  <h3 class="title">{"Quest 1: Tell Me About Yourself"}</h3>
+			<form>
+			  <div class="nes-field">
+			    <label for="instance">{"Mite Instance"}</label>
+			    <input class="nes-input" name="instance"/>
+			  </div>
+		
+			  <div class="nes-field">
+			    <label for="apikey">{"Mite API Key"}</label>
+			    <input class="nes-input" type="password" name="apikey"/>
+			  </div>
+			  
+			  <div class="nes-field">
+			    <input type="submit" class="nes-btn is-primary" value={"Complete Quest"} onclick=self.link.callback(|e| Msg::Click(e))/>
+			  </div>
+			</form>
             </div>
         }
     }
